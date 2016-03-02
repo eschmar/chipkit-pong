18b7da85099b5794fb00f92a9ce257d82e4c1f92
Marcel Eschmann <marcel.eschmann@gmail.com>
2016-03-03 00:09:13 +0100

    Improve potentiometer comments

--- helpers.c
+++ helpers.c
@@ -99,13 +99,31 @@ void enableTimer3(int period, int priority, int prescaling, int interrupts) {
  *  Set up ADC on pins A1 and A2
  */
 void setupPotentiometers() {
-    AD1PCFG = 0xFFE7;
+    // reset
     AD1CON1 = 0x0000;
+    AD1CON2 = 0x0000;
+    AD1CON3 = 0x0000;
 
-    AD1CON1 = 0x04E4;
-    AD1CON2 = 0x0406;
-    AD1CON3 = 0x0F00;
+    // ASAM: ADC Sample Auto-Start bit
+    AD1CON1SET = 0x4;
+    // SSRC: auto convert
+    AD1CON1SET = 0xE0;
+    // FORM: uint32, 0 - 1024
+    AD1CON1SET = 0x400;
 
-    AD1CSSL = 0x0110;
+    // BUFM: Result buffer mode, split in 2
+    AD1CON2SET = 0x2;
+    // SMPI: 2 sample/convert sequences between interrupts
+    AD1CON2SET = 0x4;
+    // CSCNA: Scan inputs
+    AD1CON2SET = 0x400;
+
+    // SAMC: 31 TAD
+    AD1CON3SET = 0xF00;
+
+    // CSSL: ADC Input Pin Scan Selection bits
+    AD1CSSLSET = 0x0110;
+
+    // start
     AD1CON1SET = 0x8000;
 }
